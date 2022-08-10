2714a90be74e82c7f840b667dd6b4a59287192e1
Marilene Soares da Costa <lene626@gmail.com>
2022-08-10 11:22:24 -0600

    adds RBAC and MyConfig class

---
+++ src/main/java/com/example/autos/MyConfig.java
@@ -0,0 +1,17 @@
+package com.example.autos;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class MyConfig {
+    String message;
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
