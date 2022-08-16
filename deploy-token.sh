58cd96d370d7c6fcbe13864200412790df2eccfc
Marilene Soares da Costa <lene626@gmail.com>
2022-08-16 09:50:41 -0600

    changes deployment file

--- deploy-token.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret docker-registry mdacosta-simple-autos-deploy-token \
-    --docker-username=gitlab+deploy-token-1262721 \
-    --docker-password=c0hwY250UHlNOE5zcWZ3Q3JTdW0= \
-    --docker-email=lene626@gmail.com \
-    --docker-server=registry.gitlab.com
